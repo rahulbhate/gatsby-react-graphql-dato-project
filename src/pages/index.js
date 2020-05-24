bd61130bf7212214cd2966fb49af75b365e9ebb2
rahulbhate <bhate.rahul@gmail.com>
2020-05-24 17:25:08 +1000

    minor changes in CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { graphql } from "gatsby"
 //import Img from "gatsby-image"
 import Title from "../components/title"
 import Layout from "../components/layout"
-
+//https://5eca132a40c162e62f5de8a4--eloquent-booth-2bc360.netlify.app/
 const Index = ({ data: { home } }) => (
   <Layout>
     <Title text={home.title} />
