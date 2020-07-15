ca8d4c7bf6b2a59dfd11d67be08a0ed70a027ce6
Guill Rodas <guill.rodas@gmail.com>
2020-07-15 11:32:13 +1000

    Update buyersrenters.js
    changes to Form fields to match Campaign monitor list fields

--- src/pages/buyersrenters.js
+++ src/pages/buyersrenters.js
@@ -102,18 +102,41 @@ const About = () => (
       </p>
     </div>
     <div className={styles.detailsArea}>
-    <form className="js-cm-form" id="subForm" action="https://www.createsend.com/t/subscribeerror?description=" method="post" data-id="2BE4EF332AA2E32596E38B640E9056198BDB993E3EB63847E5EF21086953903ECF9187F0F3B85CA6BE80C29E12BD1727B70FF52493C37BEC1232353481063C71">
-        <div className={styles.row}><div className={styles.column}><input aria-label="First name" id="fieldjruuiit" maxlength="200" name="cm-f-jruuiit" placeholder="First Name" /></div></div>
-        <div className={styles.row}><div className={styles.column}><input aria-label="Last Name" id="fieldjruuiij" maxlength="200" name="cm-f-jruuiij" placeholder="Last Name"/></div></div>
-        <div className={styles.row}><div className={styles.column}><input aria-label="Organisation" id="fieldjrkini" maxlength="200" name="cm-f-jrkini" placeholder="Organization"/></div></div>
-        <div className={styles.row}><div className={styles.column}><input aautocomplete="Email" aria-label="Email" class="js-cm-email-input qa-input-email" id="fieldEmail" maxlength="200" name="cm-ydukhrj-ydukhrj" required="" type="email"  placeholder="Email" /></div></div>
-        <div className={styles.row}><div className={styles.column}><input aria-label="Phone" id="fieldjrkind" maxlength="200" name="cm-f-jrkind" type="number" placeholder="Phone" /></div></div>
-        <div className={styles.row}><div className={styles.columnequals}><input aria-required="" id="cm-privacy-consent" name="cm-privacy-consent" required="" type="checkbox" /><label for="cm-privacy-consent" classname={styles.columnequals} style={{display: 'inline-block', marginLeft: '24px', verticalAlign: 'middle'}}>I agree to be emailed</label>
-        <input id="cm-privacy-consent-hidden" name="cm-privacy-consent-hidden" type="hidden" value="true"></input>
-        </div></div>
-        <div className={styles.row}>
-        <input type="submit" value="Join" />
+    <form className="js-cm-form" id="subForm" action="https://uthru.io" method="post" data-id="data-id="2BE4EF332AA2E32596E38B640E9056190134966126610899F0D95CFEBB3DFDC3750E5E25569004B6DD2688D9059E5EF5673C564979B945463D2D38A77B1E0BF5">
+
+        <div className={styles.row}><div className={styles.column}>
+                <input aria-label="Name" id="fieldName" maxlength="200" name="cm-name" required="" placeholder="First Name" />
+            </div>
         </div>
+    
+        <div className={styles.row}><div className={styles.column}>
+                <input aria-label="Last name" id="fieldjybldh" maxlength="200" name="cm-f-jybldh" required="" placeholder="Last name" />
+            </div>
+        </div>
+    
+        <div className={styles.row}><div className={styles.column}>
+            <input autocomplete="Email" aria-label="Email" class="js-cm-email-input qa-input-email" id="fieldEmail" maxlength="200" name="cm-ydukdhj-ydukdhj" required="" type="email" placeholder="Email" />
+            </div>
+        </div>
+    
+        <div className={styles.row}>
+            <div className={styles.column}>
+                <input aria-label="Phone" id="fieldjrkind" maxlength="200" name="cm-f-jrkind" type="number" placeholder="Phone" />
+                <input aria-label="Phone" id="fieldjybldk" maxlength="200" name="cm-f-jybldk" placeholder="Phone" />
+            </div>
+        </div>
+    
+        <div className={styles.row}>
+            <div className={styles.columnequals}>
+            <input aria-required="" id="cm-privacy-consent" name="cm-privacy-consent" required="" type="checkbox" /><label for="cm-privacy-consent" classname={styles.columnequals} style={{display: 'inline-block', marginLeft: '24px', verticalAlign: 'middle'}}>I agree to be emailed</label>
+            <input id="cm-privacy-consent-hidden" name="cm-privacy-consent-hidden" type="hidden" value="true"></input>
+            </div>
+        </div>
+    
+        <div className={styles.row}>
+            <input type="submit" value="Join" />
+        </div>
+    
       </form>
       <script type="text/javascript" src="https://js.createsend1.com/javascript/copypastesubscribeformlogic.js"></script>
     </div>
