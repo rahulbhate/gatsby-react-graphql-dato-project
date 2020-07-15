57df75c769fce06bb87b72b33cb632e0d3f8ccc3
Guill Rodas <guill.rodas@gmail.com>
2020-07-15 22:53:30 +1000

    Update gatsby-browser.js
    comment script

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,15 @@
 import "./src/styles/global.css"
 export { default as wrapRootElement } from "./src/redux/reduxWrapper"
+
+const addScript = url => {
+  const script = document.createElement("script")
+  script.src = url
+  script.async = true
+  document.body.appendChild(script)
+}
+
+export const onClientEntry = () => {
+  window.onload = () => {
+    addScript("https://js.createsend1.com/javascript/copypastesubscribeformlogic.js")
+   
+}
